4f6160ab72bfb917fdb193ee98dd4436bb7f2acd
root <root@vps21511.publiccloud.com.br>
2020-08-22 22:06:18 -0300

    teste

--- ren.sh
+++ ren.sh
@@ -3,16 +3,20 @@ clear
 #Usuario e Validade tabela.
 echo -e "\e[44;37;1m Usuario         Validade \e[m"
 echo ""
-
+LINHA=$(echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m")
 #Função que retorna se a validade é igual a Venceu, Nunca ou número de dias.
 for users in `awk -F : '$3 > 900 { print $1 }' /etc/passwd |sort |grep -v "nobody" |grep -vi polkitd |grep -vi system-`
 do
 datauser=$(chage -l $users |grep -i co |awk -F : '{print $2}')
-
+Usuario2=""
+dias=""
+ALIN=""
     databr="$(date -d "$datauser" +"%Y%m%d")"
     hoje="$(date -d today +"%Y%m%d")"
     if [ $hoje -ge $databr ]
     then
+    ALIN=1
+    Usuario2=$users
     dias="\033[1;31mVenceu\033[0m"
     else
     #a diferença dos dias vencimento-hoje é positiva logo existem dias restantes
@@ -24,22 +28,25 @@ datauser=$(chage -l $users |grep -i co |awk -F : '{print $2}')
            SREST=$(expr $SREN - $SHOJ)
            #foi feito a equivalência em dia dos respectivos segundos.
            DREST=$(expr $SREST / 86400)
-           echo "daleeee $DREST"
            #foi verificado a situação de alerta para avisar os clientes quando estiver próximo do dia de renovação.
            if [ $DREST -le 4 ]  
            then
-           dias=$DREST
-           echo $dias
+           dias="$DREST Dias"
+           Usuario2=$users
+           ALIN=1
            fi
     fi
     
 
 #dados mais importantes para trabalhar com api ou bot.
-Usuario=$(printf ' %-15s' "$users")
-DATA2=$(printf '%-1s' "$dias Dias")
+Usuario=$(printf ' %-15s' "$Usuario2")
+DATA=$(printf '%-1s' "$dias")
 
 #imprimi o nome do usuario e dias para expirar(dias, venceu ou nunca).
-echo -e "\033[1;33m$Usuario \033[1;32m$DATA2\033[0m"
+echo -e "$Usuario      $DATA"
 #linha sequencial que separa as informações de cada usuário.
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+if [ $ALIN=1 ]
+then
+$LINHA
+fi
 done
